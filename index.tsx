805b58c73c984e5b9104d76fe99850cae69fcbd1
Dulaj Kavinda <dulajkaavinda@gmail.com>
2023-08-13 03:51:57 +0530

    config: setting up useState

--- index.tsx
+++ index.tsx
@@ -8,19 +8,45 @@ let React = {
   },
 };
 
-const App = () => (
-  <div className="hello-classname">
-    <h1>Hello, Parent!</h1>
-    <p>Hello, from P!</p>
-  </div>
-);
+const states = [];
+let cursor = 0;
+
+const useState = (initialValue: any) => {
+  const FROZEN_CURSOR = cursor;
+  states[FROZEN_CURSOR] = states[FROZEN_CURSOR] || initialValue;
+  const setState = (newValue: any) => {
+    states[FROZEN_CURSOR] = newValue;
+    rerender();
+  };
+  return [states[FROZEN_CURSOR], setState];
+};
+
+const App = () => {
+  const [name, setName] = useState("person");
+
+  return (
+    <div className="hello-classname">
+      <h1>Hello, {name}</h1>
+      <input
+        value={name}
+        onchange={(e) => {
+          setName(e.target.value);
+        }}
+        placeholder="name"
+      />
+      <p>Hello, from P!</p>
+    </div>
+  );
+};
 
 const render = (reactElement: any, container: any) => {
   if (["string", "number"].includes(typeof reactElement)) {
     container.appendChild(document.createTextNode(String(reactElement)));
     return;
   }
+
   const actualDomElement = document.createElement(reactElement.tag);
+
   if (reactElement.props) {
     Object.keys(reactElement.props)
       .filter((key) => key !== "children")
@@ -34,6 +60,12 @@ const render = (reactElement: any, container: any) => {
   }
 
   container.appendChild(actualDomElement);
+};
+
+const rerender = () => {
+  cursor = 0;
+  document.querySelector("#app").firstChild.remove();
+  render(<App />, document.querySelector("#app"));
 };
 
 render(<App />, document.querySelector("#app"));
